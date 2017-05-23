5427ae8be6cc6f547d07fe60c11aad1791d767f9
Unknown <Frank_Schutte@hotmail.com>
2017-05-23 14:42:24 +0200

    add basic server script

--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
-/**
- * Created by Frank on 23-5-2017.
- */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/static', (req, res) => {
+    res.sendfile(path.join(__dirname, '..', 'build', 'static', req.path));
+});
+
+app.route('/')
+    .get((req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+    });
+
+app.listen(3000, () => {
+    console.log('Server started and listening to ::', 3000);
+});

b38d46c87754fddeaccb04f379425372d245fdb1
Unknown <albertveldman96@gmail.com>
2017-05-30 14:33:00 +0200

    Added sport information route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {Router, Route, browserHistory} from 'react-router';
 import App from './components/App';
 import Room from './components/Room';
 import Sports from './components/Sports';
+import Information from './components/Information';
 import NotFound from './components/NotFound';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
@@ -14,6 +15,7 @@ ReactDOM.render((
         <Route path="/" component={App}>
             <Route path="/room/:roomId" component={Room}/>
             <Route path="/sports/:roomId/:doorId" components={Sports}/>
+            <Route path="/information/:sportName" components={Information}/>
         </Route>
         <Route path="*" component={NotFound}/>
     </Router>

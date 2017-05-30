b575b43c8341e648948e7b16abdd68d8f8e79928
Unknown <albertveldman96@gmail.com>
2017-05-30 14:37:03 +0200

    Added sport information page with video

---
+++ src/components/Information/index.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {browserHistory} from 'react-router';
+
+import Youtube from 'react-youtube';
+import BackButton from '../BackButton';
+import './index.css';
+import sportsInformation from '../../sportsInformation';
+
+class Information extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sport: '',
+            sportInformation: []
+        }
+    }
+
+    componentDidMount() {
+        const sport = sportsInformation[this.props.params.sportName];
+        if (sport !== undefined)
+            this.setState({sport: this.props.params.sportName,
+                sportInformation: sport});
+    }
+
+    goBack = () => {
+        browserHistory.go(-1);
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.sport}</h1>
+                <Youtube videoId={this.state.sportInformation.videoId}/>
+                <p>{this.state.sportInformation.information}</p>
+                <BackButton goBack={this.goBack.bind(this)}/>
+            </div>
+        )
+    }
+}
+
+export default Information;

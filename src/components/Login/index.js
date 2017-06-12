f953e11b8862b6904c7f0be982e1e0b0bbf9cf73
Frank Schutte <Frank_Schutte@hotmail.com>
2017-06-12 12:47:20 +0200

    update audio in login screen

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,7 +3,6 @@ import {browserHistory} from 'react-router';
 
 import Audio from '../Audio';
 import CaptureImage from '../CaptureImage';
-import audioPath from './Begin van sportestic.mp3';
 import startButtonImage from './buttons-start.svg';
 import './index.css';
 
@@ -42,8 +41,11 @@ class Login extends Component {
         return (
             <div id="login">
                 <div id="help-audio">
-                    <Audio audioPath={audioPath}
-                           autoPlay={true}/>
+                    {(this.state.canStart) ?
+                        <Audio audioPath={process.env.PUBLIC_URL + '/audio/na het maken van foto.mp3'}
+                               autoPlay={true}/> :
+                        <Audio audioPath={process.env.PUBLIC_URL + '/audio/Begin maak foto.mp3'}
+                               autoPlay={true}/>}
                 </div>
                 <h1>Sportestic</h1>
                 <div id="login-form">
